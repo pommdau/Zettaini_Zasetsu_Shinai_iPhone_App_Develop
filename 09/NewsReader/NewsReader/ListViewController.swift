c770cd2a7c47353ebe8b16745581b2e9a0986ea2
hikeuchi <hikeuchi@morisawa.co.jp>
2019-04-05 11:36:06 +0900

    P314

--- 09/NewsReader/NewsReader/ListViewController.swift
+++ 09/NewsReader/NewsReader/ListViewController.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019年 hikeuchi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                                 for: indexPath)
+            return cell
+    }
+    
+}
