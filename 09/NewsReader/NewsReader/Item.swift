af687b67f26a9450baa44a9c475d91dbbc8e2d81
hikeuchi <hikeuchi@morisawa.co.jp>
2019-04-09 11:09:22 +0900

    P329

---
+++ 09/NewsReader/NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by HIROKI IKEUCHI on 2019/04/09.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
